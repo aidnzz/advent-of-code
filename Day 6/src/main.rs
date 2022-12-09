029e2976fd9dc7abc867350d4529430c389cc103
Aidan W <mangowalk04@gmail.com>
2022-12-09 13:55:21 +0000

    Update main.rs

--- Day 6/src/main.rs
+++ Day 6/src/main.rs
@@ -1,13 +1,14 @@
-#![feature(array_windows)]
-
 fn main() {
     const INPUT: &[u8] = include_bytes!("input.txt");
     
-    for (index, elements) in INPUT.array_windows::<4>().enumerate() {
-        let duplicate = elements.iter().any(|e| elements.iter().filter(|&n| e == n).count() > 1);
-        if !duplicate {
-            println!("First marker at: {}", index + 4);
-            break;
-        }
-    }
+    let position = INPUT
+        .windows(4)
+        .position(|elements| {
+            !elements
+                .iter()
+                .any(|e| elements.iter().filter(|&n| e == n).count() > 1)
+        })
+        .map(|n| n + 4);
+    
+    println!("{}", position.unwrap());
 }
