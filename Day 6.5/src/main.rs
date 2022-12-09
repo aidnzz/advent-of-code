92abdea94380b228ad2ebd46fb644e0514646dce
Aidan W <mangowalk04@gmail.com>
2022-12-09 13:54:11 +0000

    Update main.rs

--- Day 6.5/src/main.rs
+++ Day 6.5/src/main.rs
@@ -1,16 +1,14 @@
-#![feature(array_windows)]
-
 fn main() {
     const INPUT: &[u8] = include_bytes!("input.txt");
     
     let position = INPUT
-        .windows(4)
+        .windows(14)
         .position(|elements| {
             !elements
                 .iter()
                 .any(|e| elements.iter().filter(|&n| e == n).count() > 1)
         })
-        .map(|n| n + 4);
+        .map(|n| n + 14);
     
     println!("{}", position.unwrap());
 }
